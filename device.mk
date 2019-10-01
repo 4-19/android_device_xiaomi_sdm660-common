f7c14fc2ae03062cdff56381f31a3e5ce7b131ce
PIPIPIG233666 <pig.priv@gmail.com>
2019-10-01 09:20:13 -0400

    whyred: Build vendor variant of vendor.qti.hardware.camera.device@1.0

--- device.mk
+++ device.mk
@@ -174,7 +174,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.5 \
     libdng_sdk.vendor \
-    vendor.qti.hardware.camera.device@1.0
+    vendor.qti.hardware.camera.device@1.0.vendor
 
 # Connectivity Engine support (CNE)
 PRODUCT_PACKAGES += \
